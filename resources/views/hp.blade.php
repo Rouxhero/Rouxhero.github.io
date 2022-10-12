68676ec33317d64b536a3886116341759b32cc74
Leo Le Van Canh dit Ban <leo.levancanhditban.etu@univ-lille.fr>
2022-10-12 21:32:31 +0200

    test

--- resources/views/hp.blade.php
+++ resources/views/hp.blade.php
@@ -4,6 +4,7 @@
 <script src="/js/map.js"></script>
 <script type='text/javascript' src='https://unpkg.com/leaflet.markercluster@1.3.0/dist/leaflet.markercluster.js'></script>
 <link rel="stylesheet" type="text/css" href="https://unpkg.com/leaflet.markercluster@1.3.0/dist/MarkerCluster.Default.css" />
+<h1> I WANT My COFFEE </h1>
 <div id="map" class="w-[400px] h-[400px]">
     <!-- Ici s'affichera la carte -->
 </div>
